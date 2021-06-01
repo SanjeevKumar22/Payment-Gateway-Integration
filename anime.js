32344733c935f98a9d4dbc23c22cc94e105bc17a
SanjeevKumar <msanjeev.2000@gmail.com>
2021-06-01 21:10:18 +0530

    Add files via upload

---
+++ anime.js
@@ -0,0 +1,20 @@
+// const firstHeading = document.querySelector(".first-heading");
+// const firstsubtext = document.querySelector(".first-subtext");
+// const button = document.getElementById("donate")
+// const nav = document.getElementById("navbar")
+// // const t1 = new TimelineMax({ delay: 0.2 });
+
+
+// window.onload = () => {
+//     t1.fromTo(nav, 0.5, { y: -50, opacity: 0 }, { y: 0, opacity: 1 });
+//     if (window.innerWidth < 600) {
+//         firstHeading.classList.toggle("animate");
+//         firstsubtext.classList.toggle("animate2");
+//     } else {
+//         t1.fromTo(firstHeading, 0.5, { y: -50, opacity: 0 }, { y: 0, opacity: 1 });
+//         t1.fromTo(firstsubtext, 0.5, { y: 50, opacity: 0 }, { y: 0, opacity: 1 });
+//     }
+//     t1.fromTo(button, 0.5, { opacity: 0 }, { opacity: 1 });
+// }
+
+// console.log(window.innerWidth)
